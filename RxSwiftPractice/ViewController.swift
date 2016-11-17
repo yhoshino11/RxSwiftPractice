1f10b3734c8e7a5e631ba7e0e9ce42e82e88b663
Yu Hoshino <yhoshino11@gmail.com>
2016-11-17 23:38:59 +0900

    Initial Commit

---
+++ RxSwiftPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftPractice
+//
+//  Created by yhoshino11 on 11/17/16.
+//  Copyright © 2016 yhoshino11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
